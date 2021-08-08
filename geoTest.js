e2115cdcdb405462738ff3033e7fde25166cb9e9
Joey Chou <cloudyruer@gmail.com>
2021-08-08 19:17:50 +0800

    0808 init

---
+++ geoTest.js
@@ -0,0 +1,67 @@
+const titleText = document.querySelector('.title');
+const latText = document.querySelector('.lat');
+const lngText = document.querySelector('.lng');
+const cityText = document.querySelector('.city');
+const localityText = document.querySelector('.locality');
+
+// 瀏覽器API 取得當前地理位置
+const newWhereAmI = () =>
+  new Promise((resolve, reject) => {
+    // NOTE 此為設定檔 要在討論
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+
+    navigator.geolocation.getCurrentPosition(resolve, reject, options);
+  });
+
+const geoReverse = ([lat, lng]) => {
+  fetch(
+    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=zh`
+  )
+    .then(res => res.json())
+    .then(data => {
+      console.log('--geoReverse Success--');
+      console.log(data);
+      const { latitude: lat } = data; //所在區域的緯度
+      const { longitude: lng } = data; //所在區域的經度
+      const { city } = data; //所在城市 e.g.台北市
+      const { locality } = data; //所在區  e.g. 中正區
+      console.log(lat, lng, city, locality);
+
+      // from newWhereAmI
+      latText.innerText = lat.toFixed(3);
+      lngText.innerText = lng.toFixed(3);
+      // from geoReverse
+      cityText.innerText = city;
+      localityText.innerText = locality;
+      // for title
+      titleText.innerText = '您的位置是在:';
+    });
+};
+
+const renderLocationInfo = () => {
+  // NOTE 沒有設定reject
+  newWhereAmI()
+    .then(pos => {
+      const { latitude: lat } = pos.coords; // 緯度 -> 重新命名為 lat
+      const { longitude: lng } = pos.coords; //經度 -> 重新命名為 lng
+      const { accuracy } = pos.coords; //準確度(誤差值) 以meters(公尺)為單位
+      //   const { latitude: lat, longitude: lng, accuracy } = pos.coords;
+      console.log('By Promise:');
+      console.log(`當前緯度: ${lat}`);
+      console.log(`當前經度: ${lng}`);
+      console.log(`當前誤差值: ${accuracy}(單位米)`);
+      // return [lat.toFixed(5), lng.toFixed(5)];
+      return [lat, lng];
+    })
+    .then(res => geoReverse(res));
+};
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', () => {
+  titleText.innerText = '計算中';
+  renderLocationInfo();
+});
